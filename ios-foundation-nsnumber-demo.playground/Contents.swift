77a26776bf7b6a59bf4ed44ef9854e6ad8577fd6
taguchi-k <k.taguchi@st-ventures.jp>
2017-02-21 22:19:50 +0900

    Add SampleCode

---
+++ ios-foundation-nsnumber-demo.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let number: NSNumber = 3.14
+
+/*
+ NSNumberはInt, Float, Double, Bool型へ変換可能
+ */
+
+// Int
+let int = number as Int
+type(of: int)
+
+// Float
+let float = number as Float
+type(of: float)
+
+// Double
+let double = number as Double
+type(of: double)
+
+// Bool
+let bool = number as Bool
+type(of: bool)
